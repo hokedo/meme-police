1eb846930c0ebb1a01662526b885e0981ad5c8d1
Bogdan Mic <bogdan.mic@outlook.com>
2021-11-03 20:16:52 +0200

    Added image downloaders

---
+++ meme_police/downloaders.py
@@ -0,0 +1,30 @@
+from urllib.parse import urlparse, urljoin
+
+import requests
+
+CDN_IMAGES_URL_9GAG = 'https://img-9gag-fun.9cache.com/photo/'
+CDN_IMAGES_URL_HUGELOL = 'https://hugelolcdn.com/i/'
+
+
+def image_downloader_9gag(url):
+    post_id = urlparse(url).path.split('/')[-1]
+    image_url = urljoin(CDN_IMAGES_URL_9GAG, f'{post_id}_700b.jpg')
+    response = requests.get(image_url)
+
+    if response.ok:
+        return response.content
+
+
+def image_downloader_hugelol(url):
+    post_id = urlparse(url).path.split('/')[-1]
+    image_url = urljoin(CDN_IMAGES_URL_HUGELOL, f'{post_id}.jpg')
+    response = requests.get(image_url)
+
+    if response.ok:
+        return response.content
+
+
+DOMAIN_IMAGE_DOWNLOADERS_MAP = {
+    '9gag.com': image_downloader_9gag,
+    'hugelol.com': image_downloader_hugelol,
+}
