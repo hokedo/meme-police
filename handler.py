b8997a4a14b409eae4c1effbe221f325fe789e98
Bogdan Mic <bogdan.mic@outlook.com>
2022-04-10 20:02:29 +0300

    Log all handler responses

--- handler.py
+++ handler.py
@@ -14,25 +14,30 @@ from meme_police.telegram import handle_incoming_message, parse_telegram_webhook
 logging.basicConfig(format="%(levelname)s\t%(asctime)s\t%(module)s\t%(message)s")
 
 logging.getLogger().setLevel(logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 def check_duplicate_meme_handler(event, context):
+    response = {
+        'statusCode': 200,
+    }
+
     try:
+        logger.info(f"Received event:\t{event}")
         request_body = json.loads(event['body'])
         parsed_body = parse_telegram_webhook_body(request_body)
 
         if all([parsed_body['text'], parsed_body['chat_id'], parsed_body['message_id']]):
             handle_incoming_message(parsed_body)
 
-        return {
-            'statusCode': 200,
-            # 'body': json.dumps(result)
-        }
     except Exception as er:
-        return {
+        response = {
             'statusCode': 400,
             'body': json.dumps({
                 'error': str(er),
                 'traceback': traceback.format_exc()
             })
         }
+
+    logger.info(f"Handler Response:\t{json.dumps(response)}")
+    return response
